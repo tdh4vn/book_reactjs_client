765a9a3f87be2437b2ceae95298c7bd85f0909c6
HungTran <tdh4vn@gmail.com>
2017-05-04 10:08:30 +0700

    change sv

--- src/configs/APIConfig.js
+++ src/configs/APIConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //API_URL : "https://demobookmanagement.herokuapp.com/",
-     API_URL : "http://localhost:1337/",
+    API_URL : "https://demobookmanagement.herokuapp.com/",
+    // API_URL : "http://localhost:1337/",
 }
