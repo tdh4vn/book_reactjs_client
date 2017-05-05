5de13b2107b84b4023095bdf2fbe0be6241b70ea
root <root@DESKTOP-G1V3SN6.localdomain>
2017-05-05 11:03:45 +0700

    rebuild reactjs

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
